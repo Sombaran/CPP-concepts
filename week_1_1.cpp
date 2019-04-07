cb6f40faab4058cb984abf55a8994fd2032e2672
Sombaran Gupta <40040729+Sombaran@users.noreply.github.com>
2019-04-07 16:15:46 +0530

    Create week_1_1.cpp
    The code simply show cpp style to add two strings

---
+++ week_1_1.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Brief: Week_1 basics of cpp
+ */
+#include <iostream>
+#include <string>
+
+int main(void) {
+	std::string str1 = "hello";
+	std::string str2 = "world";
+	std::string str = str1 + str2;
+	std::cout << "The added string is " << str << std::endl;
+	return(0); 
+}
