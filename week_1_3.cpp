2584ac259f2421be5c894610dad0971e1f7f7eb2
Sombaran <guptasombaran@yahoo.com>
2020-04-26 13:44:35 +0530

    201800B

--- week_1_3.cpp
+++ week_1_3.cpp
@@ -5,8 +5,16 @@ int main(void)
 {
 std::string checkString;
 
+if (nullptr == checkString)
+{
+  std::cout << " WARNING " << std::endl;
+}
+checkString = " I read and right
+She felt so light and we will unite
+We will fight but again reunite coz
+u in my right and will be my wife ";
 
-std::cout << checkString << std::endl;
+std::cout << checkString.c_str() << std::endl;
 
   return (0);
 }
