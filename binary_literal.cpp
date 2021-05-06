68452cd84b563f738061697e980d41f488243e54
Sombaran Gupta <40040729+Sombaran@users.noreply.github.com>
2021-05-06 21:22:21 +0530

    Update binary_literal.cpp

--- binary_literal.cpp
+++ binary_literal.cpp
@@ -1,3 +1,4 @@
+
 /*
  * @Brief: Like in hexadecimal and octal numbers, 
  * now we can directly write binary literals (of the form 0’s and 1’s) in C++14. 
@@ -7,16 +8,19 @@
  
 #include <iostream>
 
-auto binaryLiteral = [](auto valueReceive)
+auto genericLiteral = [](auto valueReceived)
 {
-	return 	valueReceive;
+	return 	valueReceived;
 };
 
 int main()
 {
 	uint binVar_1 {0B1111'1111}, binVar_2 {0b1111'1111}; //binary literal
-	std::cout << "Binary literal with prefix /'0B/' " << binaryLiteral(binVar_1) << std::endl;
-	std::cout << "Binary literal with prefix /'0b/' " << binaryLiteral(binVar_2) << std::endl;
-	binaryLiteral(binVar_2);
+	uint hexaVal {0x13ac};
+  uint octaVal {0117}; 
+	std::cout << "Binary literal with prefix /'0B/' " << genericLiteral(binVar_1) << std::endl;
+	std::cout << "Binary literal with prefix /'0b/' " << genericLiteral(binVar_2) << std::endl;
+	std::cout << "Hexadecimal literal with prefix /'0x/' " << genericLiteral(hexaVal) << std::endl;
+	std::cout << "Octal literal with prefix /'0/' " << genericLiteral(octaVal) << std::endl;
 	return (0);
 }
