b7640c845ef0c2bcfb27aa5bf649af0afd0cf3fd
Sombaran Gupta <40040729+Sombaran@users.noreply.github.com>
2021-05-07 01:17:46 +0530

    Update return_auto_type_deduction.cpp

--- return_auto_type_deduction.cpp
+++ return_auto_type_deduction.cpp
@@ -47,16 +47,16 @@ int main()
 {
 	uint var_1 {4200u}, var_2{1356u};
 	std::cout << "Simple auto return type deduction: --> USING LAMBDA__SIMPLE____1 " << autoReturnTypeLambda_1(var_1, var_2) << std::endl; 
-  std::cout << "Simple auto return type deduction: --> USING LAMBDA__REFERENCE_2 " << autoReturnTypeLambda_2(var_1, var_2) << std::endl;
-  const uint& var_3 {var_2};
-  std::cout << "Simple auto return type deduction: --> USING LAMBDA__REFERENCE_3 " << autoReturnTypeLambda_3(var_3) << std::endl;
-  std::cout << std::endl;
-  uint var_4 {1947};
-  std::cout << "Simple auto return type deduction: --> USING AUTOTYPEDEDUCTION__NO__REFERENCE_1 " << returnAutoTypeDeduction_1(var_4) << std::endl;
-  const uint& var_5{var_4};
-  std::cout << "Simple auto return type deduction: --> USING AUTOTYPEDEDUCTION______REFERENCE_2 " << returnAutoTypeDeduction_2(var_5) << std::endl;
-  //remove const usibg const_Cast
-  uint finalVal = const_cast<uint& > (var_5);
-  std::cout << "Simple auto return type deduction: --> USING AUTOTYPEDEDUCTION______REFERENCE_3 " << returnAutoTypeDeduction_3(finalVal) << std::endl;
+  	std::cout << "Simple auto return type deduction: --> USING LAMBDA__REFERENCE_2 " << autoReturnTypeLambda_2(var_1, var_2) << std::endl;
+  	const uint& var_3 {var_2};
+  	std::cout << "Simple auto return type deduction: --> USING LAMBDA__REFERENCE_3 " << autoReturnTypeLambda_3(var_3) << std::endl;
+  	std::cout << std::endl;
+  	uint var_4 {1947};
+  	std::cout << "Simple auto return type deduction: --> USING AUTOTYPEDEDUCTION__NO__REFERENCE_1 " << returnAutoTypeDeduction_1(var_4) << std::endl;
+  	const uint& var_5{var_4};
+  	std::cout << "Simple auto return type deduction: --> USING AUTOTYPEDEDUCTION______REFERENCE_2 " << returnAutoTypeDeduction_2(var_5) << std::endl;
+  	//remove const usibg const_Cast
+  	uint finalVal = const_cast<uint& > (var_5);
+  	std::cout << "Simple auto return type deduction: --> USING AUTOTYPEDEDUCTION______REFERENCE_3 " << returnAutoTypeDeduction_3(finalVal) << std::endl;
 	return (0);
 }
